f6ecf64fef94165494c352ad6faa413169217af9
Jonathan Holtby <holtby.jonathan@gmail.com>
2019-02-04 07:51:38 +0000

    Add files via upload

--- mam_api_v1_sdk/__init__.py
+++ mam_api_v1_sdk/__init__.py
@@ -1,22 +1,35 @@
-"""
-	Mark and Maven API
-	Contact: systems@markandmaven.com
-"""
+from __future__ import absolute_import
 
-# import apis into the sdk package
-from mam_api_v1_sdk.api.abilities_api import AbilitiesApi
-from mam_api_v1_sdk.api.account_api import AccountApi
-from mam_api_v1_sdk.api.admin_api import AdminApi
-from mam_api_v1_sdk.api.attributes_api import AttributesApi
-from mam_api_v1_sdk.api.contacts_api import ContactsApi
-from mam_api_v1_sdk.api.demos_api import DemosApi
-from mam_api_v1_sdk.api.skills_api import SkillsApi
 
-# import ApiClient
-from mam_api_v1_sdk.api_client import ApiClient
-from mam_api_v1_sdk.configuration import Configuration
-# import models into sdk package
-from mam_api_v1_sdk.models.create_account import CreateAccount
-from mam_api_v1_sdk.models.create_resume import CreateResume
-from mam_api_v1_sdk.models.get_account import GetAccount
-from mam_api_v1_sdk.models.get_resume import GetResume
+
+# flake8: noqa
+
+
+
+# import apis into api package
+
+from sib_api_v3_sdk.api.account_api import AccountApi
+
+from sib_api_v3_sdk.api.attributes_api import AttributesApi
+
+from sib_api_v3_sdk.api.contacts_api import ContactsApi
+
+from sib_api_v3_sdk.api.email_campaigns_api import EmailCampaignsApi
+
+from sib_api_v3_sdk.api.folders_api import FoldersApi
+
+from sib_api_v3_sdk.api.lists_api import ListsApi
+
+from sib_api_v3_sdk.api.process_api import ProcessApi
+
+from sib_api_v3_sdk.api.reseller_api import ResellerApi
+
+from sib_api_v3_sdk.api.sms_campaigns_api import SMSCampaignsApi
+
+from sib_api_v3_sdk.api.smtp_api import SMTPApi
+
+from sib_api_v3_sdk.api.senders_api import SendersApi
+
+from sib_api_v3_sdk.api.transactional_sms_api import TransactionalSMSApi
+
+from sib_api_v3_sdk.api.webhooks_api import WebhooksApi
