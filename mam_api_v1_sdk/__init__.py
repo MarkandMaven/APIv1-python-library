f00ebc7e6f7d422a7ad4c96dd30e81d745decfb4
Jonathan Holtby <holtby.jonathan@gmail.com>
2019-02-04 08:26:00 +0000

    Add files via upload

--- mam_api_v1_sdk/__init__.py
+++ mam_api_v1_sdk/__init__.py
@@ -3,20 +3,8 @@
 	Contact: systems@markandmaven.com
 """
 
-# import apis into the sdk package
-from mam_api_v1_sdk.api.abilities_api import AbilitiesApi
-from mam_api_v1_sdk.api.account_api import AccountApi
-from mam_api_v1_sdk.api.admin_api import AdminApi
-from mam_api_v1_sdk.api.attributes_api import AttributesApi
-from mam_api_v1_sdk.api.contacts_api import ContactsApi
-from mam_api_v1_sdk.api.demos_api import DemosApi
-from mam_api_v1_sdk.api.skills_api import SkillsApi
-
-# import ApiClient
-from mam_api_v1_sdk.api_client import ApiClient
-from mam_api_v1_sdk.configuration import Configuration
-# import models into sdk package
-from mam_api_v1_sdk.models.create_account import CreateAccount
-from mam_api_v1_sdk.models.create_resume import CreateResume
-from mam_api_v1_sdk.models.get_account import GetAccount
-from mam_api_v1_sdk.models.get_resume import GetResume
+# import models into model package
+from mam_api_v1_sdk.models.createAccount import CreateAccount
+from mam_api_v1_sdk.models.createResume import CreateResume
+from mam_api_v1_sdk.models.getAccount import GetAccount
+from mam_api_v1_sdk.models.getResume import GetResume
